9454a2f847dfdf8c7d4149dbdc6b8e5a0474f5c1
Sasha Fesenko <sashafesenkooo@gmail.com>
2023-01-13 09:25:54 +0200

    add tast 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+console.log(createGalleryMarkup(galleryItems));
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(items) {
+  const markup = items.map(({ preview, original, description }) => {
+    return `
+    
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a> `
+  });
+ 
+  return markup.join('');
+}
+
+function instalSimpleLightbox() {
+  let lightbox  = new SimpleLightbox('.gallery .gallery__item', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+  }
+
+  instalSimpleLightbox();
