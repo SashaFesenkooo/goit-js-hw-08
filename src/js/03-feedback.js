97d56fd8163d6f5f8bd4bac319b44943caf5988a
Sasha Fesenko <sashafesenkooo@gmail.com>
2023-01-16 15:11:17 +0200

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,50 @@
-import throttle from 'lodash.throttle'; 
-  
- const formEl = document.querySelector('.feedback-form'); 
- const emailInputEl = document.querySelector('input'); 
- const messageInputEl = document.querySelector('textarea'); 
-  
- formEl.addEventListener('input', throttle(dataFormEl, 500)); 
- formEl.addEventListener('submit', onFormSubmit); 
-  
- const STORAGE_KEY_DATAFORM = 'feedback-form-state'; 
-  
- let formData = JSON.parse(localStorage.getItem(STORAGE_KEY_DATAFORM)) || {}; 
-  
- auditLocalStorage(); 
-  
- function dataFormEl(evt) { 
-   formData[evt.target.name] = evt.target.value; 
-   //   console.log(formData); 
-   localStorage.setItem(STORAGE_KEY_DATAFORM, JSON.stringify(formData)); 
- } 
-  
- function onFormSubmit(evt) { 
-   evt.preventDefault(); 
-   console.log(formData); 
-   evt.currentTarget.reset(); 
-   localStorage.removeItem(STORAGE_KEY_DATAFORM); 
-   formData = {}; 
- } 
-  
- function auditLocalStorage() { 
-   const savedFormData = localStorage.getItem(STORAGE_KEY_DATAFORM); 
-   const parsedFormData = JSON.parse(savedFormData); 
+import throttle from 'lodash.throttle'; 
+
+const refs = {
+    formEl: document.querySelector('.feedback-form'),
+    inputEl: document.querySelector('input'),
+    messageInput: document.querySelector('textarea'),
+};
+
+const STORAGE_KEY = 'form-message';
+
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+populateTextarea();
+
+refs.formEl.addEventListener('input', throttle(dataFormEl, 500));
+refs.formEl.addEventListener('submit', onFormSubmit)
+
+function dataFormEl(evt) {
+    const message = evt.target.value;
+
+   
+    formData[evt.target.name] = message;
+ 
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+}
+
+function onFormSubmit(evt) {
+    
+    evt.preventDefault();
+   
+    if (formData.email && formData.message) {
+        console.log(formData);
+        evt.target.reset();
+        Object.keys(formData).forEach(key => delete formData[key]);
+        localStorage.removeItem(STORAGE_KEY);
+
+        return;
+    } 
+};
+
+function populateTextarea() {
+    const savedmessage = localStorage.getItem(STORAGE_KEY);
+
+    const parsedFormData = JSON.parse(savedmessage); 
   
    if (parsedFormData) { 
-     emailInputEl.value = formData.email || ''; 
-     messageInputEl.value = formData.message || ''; 
+ refs.inputEl.value = formData.email || ''; 
+ refs.messageInput.value = formData.message || ''; 
    } 
- }
+};
